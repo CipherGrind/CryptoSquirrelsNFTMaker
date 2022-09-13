be439c3e6df6b08199d9822e2eeaeea46ee56bb6
CipherGrind <101846763+CipherGrind@users.noreply.github.com>
2022-09-13 07:35:49 -0400

    update2

--- assets/js/catFactory.js
+++ assets/js/catFactory.js
@@ -14,21 +14,11 @@ function genColors(){
     return colors
 }
 
-
-//This function code needs to modified so that it works with Your cat code.
-function headColor(color,code) {
-    $('.face, .leftCheek, .rightCheek, .belly, .innertail2').css('background-color', '#' + color)  //This changes the color of the cat
-    $('#headcode').html('code: '+code) //This updates text of the badge next to the slider
-    $('#dnahead').html(code) //This updates the body color part of the DNA that is displayed below the cat
-    console.log($('.face').css('background-color'))
-    var headAndBellyColorRBG = ($('.face').css('background-color'))
-    $('#headAndBellyColorRBG').html(headAndBellyColorRBG)
-}
-
-
-//This function code needs to modified so that it works with Your cat code.
+//###################################################
+//BODYLIMBSFACE FUNCTION
+//###################################################
 function bodyLimbsColor(color,code) {
-    $('#body, .leftArm, .rightArm, .leftThigh, .rightThigh, .leftFoot, .rightFoot, #tail')
+    $('#body, .face, .leftEar, .rightEar, .leftArm, .rightArm, .leftThigh, .rightThigh, .leftFoot, .rightFoot, #tail, .innertail2')
         .css('background-color', '#' + color)  //This changes the color of the cat
     $('#bodycode').html('code: '+code) //This updates text of the badge next to the slider
     $('#dnabody').html(code) //This updates the body color part of the DNA that is displayed below the cat
@@ -38,19 +28,34 @@ function bodyLimbsColor(color,code) {
 }
 
 
-//This function code needs to modified so that it works with Your cat code.
-function tailEarsColor(color,code) {
-    $('.leftEar, .rightEar, .toe1, .toe2, .toe3, .toe4, .finger1, .finger2, .innertail, .innertail3')
+//###################################################
+//TAILBELLY FUNCTION
+//###################################################
+function tailBellyColor(color,code) {
+    $('.belly, .innerBelly, .innertail, .innertail3, .eye1, .eye2, .toe1, .toe2, .toe3, .toe4, .finger1, .finger2')
         .css('background-color', '#' + color)  //This changes the color of the cat
     $('#tailcode').html('code: '+code) //This updates text of the badge next to the slider
     $('#dnatail').html(code) //This updates the body color part of the DNA that is displayed below the cat
     console.log($('#tail').css('background-color'))
-    var tailAndEarsColorRBG = ($('#tail').css('background-color'))
-    $('#tailAndEarsColorRBG').html(tailAndEarsColorRBG)
+    var tailBellyColorRBG = ($('#tail').css('background-color'))
+    $('#tailBellyColorRBG').html(tailBellyColorRBG) 
 }
 
+//###################################################
+//SURFBOARD FUNCTION
+//###################################################
+function surfBoardColor(color,code) {
+    $('.surfBoard, .surfBoardFin, .surfBoardFin2').css('background-color', '#' + color)  //This changes the color of the cat
+    $('#surfcode').html('code: '+code) //This updates text of the badge next to the slider
+    $('#dnasurf').html(code) //This updates the body color part of the DNA that is displayed below the cat
+    console.log($('.surfBoard').css('background-color'))
+    var surfBoardColorRBG = ($('.surfBoard').css('background-color'))
+    $('#surfBoardColorRBG').html(surfBoardColorRBG)
+}
 
-//This function code needs to modified so that it works with Your cat code.
+//###################################################
+//BACKGROUND FUNCTION
+//###################################################
 function bgColor(color,code) {
     $('.squirrelBox')
         .css('background-color', '#' + color)  //This changes the color of the cat
@@ -63,39 +68,267 @@ function bgColor(color,code) {
 
 
 
-
 //###################################################
-//Functions below will be used later on in the project
+//EYE FUNCTION
 //###################################################
 function eyeVariation(num) {
-
-    $('#dnashape').html(num)
+    
+    $('#dnaeyes').html(num)
     switch (num) {
         case 1:
             normalEyes()
-            $('#eyeName').html('Basic')
+            $('#eyeName').html('Normal') //set the badge to "n0rmal"
+            break
+        case 2:
+            normalEyes() //reset 
+            $('#eyeName').html('Chill')
+            eyesType2() //set border type
+            break
+        case 3:
+            normalEyes() //reset 
+            $('#eyeName').html('Shocked')
+            eyesType3() //set border type
+            break
+        case 4:
+            normalEyes() //reset 
+            $('#eyeName').html('Stunned')
+            eyesType4() //set border type
+            break
+        case 5:
+            normalEyes() //reset 
+            $('#eyeName').html('Scared')
+            eyesType5() //set border type
+            break
+        case 6:
+            normalEyes() //reset 
+            $('#eyeName').html('Confused')
+            eyesType6() //set border type
+            break
+        case 7:
+            normalEyes() //reset 
+            $('#eyeName').html('Squint')
+            eyesType7() //set border type
+            break
+        case 8:
+            normalEyes() //reset 
+            $('#eyeName').html('Woah')
+            eyesType8() //set border type
+            break
+        case 9:
+            normalEyes() //reset 
+            $('#eyeName').html('Tiny')
+            eyesType9() //set border type
             break
     }
 }
 
-function decorationVariation(num) {
-    $('#dnadecoration').html(num)
+function normalEyes() {
+    $('.eyes').find('.pupils, .innerPupils').css('border-style', 'double')
+    $('.eyes').find('.eye1, .eye2').css('transform', 'skew(0deg)')
+    $('.eyes').find('.eye1, .eye2').css('margin', '10px')
+    $('.eyes').find('.eye1, .eye2').css('top', '0px')
+    $('.eyes').find('.pupils, .innerPupils').css('margin-left', '0px')
+    $('.eyes').find('.pupils, .innerPupils').css('margin-right', '0px')
+}
+function eyesType2() {
+    $('.eyes').find('.pupils, .innerPupils').css('border-style', 'solid')
+    $('.eyes').find('.pupils, .innerPupils').css('border-width', '7px')
+    $('.eyes').find('.eye1, .eye2').css('transform', 'skew(0deg)')
+}
+function eyesType3() {
+    $('.eyes').find('.innerPupils').css('border-style', 'dotted')
+    $('.eyes').find('.eye1, .eye2').css('transform', 'skew(0deg)')
+}
+function eyesType4() {
+    $('.eyes').find('.innerPupils').css('border-style', 'dashed')
+    $('.eyes').find('.eye1, .eye2').css('transform', 'skew(0deg)')
+}
+function eyesType5() {
+    $('.eyes').find('.eye1, .eye2').css('transform', 'scaleX(1) scaleY(1)')
+    $('.eyes').find('.eye1, .eye2').css('top', '-10px')
+    $('.eyes').find('.eye1').css('margin-left', '20px')
+    $('.eyes').find('.pupils, .innerPupils').css('margin-left', '10px')
+    $('.eyes').find('.eye2').css('margin-right', '20px')
+    $('.eyes').find('.pupils, .innerPupils').css('margin-right', '10px')
+}
+function eyesType6() {
+    $('.eyes').find('.eye1').css('transform', 'skew(-10deg) scaleX(1) scaleY(1)')
+    $('.eyes').find('.eye2').css('transform', 'skew(-10deg) scaleX(.75) scaleY(.75)')
+}
+function eyesType7() {
+    $('.eyes').find('.eye1, .eye2').css('transform', 'scaleY(.25)')
+}
+function eyesType8() {
+    $('.eyes').find('.eye1, .eye2').css('transform', 'scaleX(1.2) scaleY(1.2)')
+}
+function eyesType9() {
+    $('.eyes').find('.eye1, .eye2').css('transform', 'scaleX(.6) scaleY(.6)')
+}
+
+
+//###################################################
+//TAILSPIN FUNCTION
+//###################################################
+function tailspinVariation(num) {
+    
+    $('#dnatailspin').html(num)
     switch (num) {
         case 1:
-            $('#decorationName').html('Basic')
-            normaldecoration()
+            normalTailspin()
+            $('#TailspinName').html('Normal') //set the badge to "n0rmal"
+            break
+        case 2:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Excited')
+            TailspinType2() //set border type
+            break
+        case 3:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Enlarged')
+            TailspinType3() //set border type
+            break
+        case 4:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Twirled')
+            TailspinType4() //set border type
+            break
+        case 5:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Stretched')
+            TailspinType5() //set border type
+            break
+        case 6:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Fattened')
+            TailspinType6() //set border type
+            break
+        case 7:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Fanned')
+            TailspinType7() //set border type
+            break
+        case 8:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Stiff')
+            TailspinType8() //set border type
+            break
+        case 9:
+            normalTailspin() //reset 
+            $('#TailspinName').html('Tiny')
+            TailspinType9() //set border type
             break
     }
 }
 
-async function normalEyes() {
-    await $('.cat__eye').find('span').css('border', 'none')
+function normalTailspin() {
+    $('#tail').css('transform', 'scale(-1, 1) rotate(-65deg)')
+    $('.innerTail, .innerTail2, .innerTail3').css('transform', 'none')
+    $('#tail').css('top', '260px')
+}
+function TailspinType2() {
+    $('#tail, .innerTail, .innerTail2, .innerTail3').css('transform', 'skew(-15deg) scaleX(1.05) scaleY(1.1)')
+}
+function TailspinType3() {
+    $('#tail, .innerTail, .innerTail2, .innerTail3').css('transform', 'skew(-5deg) rotate(10deg) scaleX(1.25) scaleY(1.25)')
+    $('#tail').css('top', '250px')
+}
+function TailspinType4() {
+    $('#tail, .innerTail, .innerTail2, .innerTail3').css('transform', 'skew(10deg) rotate(24deg) scaleX(1.1) scaleY(1.1)')
+    $('#tail').css('top', '280px')
+}
+function TailspinType5() {
+    $('#tail, .innerTail, .innerTail2, .innerTail3').css('transform', 'skew(-8deg) rotate(13deg) scaleX(.9) scaleY(1.4)')
+    $('#tail').css('top', '240px')
+}
+function TailspinType6() {
+    $('#tail').css('transform', 'skew(10deg) rotate(15deg) scaleX(1.75) scaleY(1.2)')
+    $('.innerTail').css('transform', 'skew(-10deg) rotate(-10deg) scaleX(1) scaleY(1)')
+
+}
+function TailspinType7() {
+    $('#tail').css('transform', 'skew(-10deg) rotate(10deg) scaleX(1.1) scaleY(1.1)')
+    $('.innerTail').css('transform', 'skew(-10deg) rotate(15deg) scaleX(1.1) scaleY(1.1)')
+    $('.innerTail2').css('transform', 'skew(-10deg) rotate(20deg) scaleX(1.1) scaleY(1.1)')
+    $('.innerTail3').css('transform', 'skew(-10deg) rotate(5deg) scaleX(1.0) scaleY(1.0)')
+}
+function TailspinType8() {
+    $('#tail').css('transform', 'skew(-60deg) rotate(40deg) scaleX(.75) scaleY(.75)')
+    $('#tail').css('top', '300px')
+}
+function TailspinType9() {
+    $('#tail').css('transform', 'skew(30deg) rotate(70deg) scaleX(.8) scaleY(.8)')
+    $('#tail').css('top', '325px')
 }
 
-async function normaldecoration() {
-    //Remove all style from other decorations
-    //In this way we can also use normalDecoration() to reset the decoration style
-    $('.cat__head-dots').css({ "transform": "rotate(0deg)", "height": "48px", "width": "14px", "top": "1px", "border-radius": "0 0 50% 50%" })
-    $('.cat__head-dots_first').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "50% 0 50% 50%" })
-    $('.cat__head-dots_second').css({ "transform": "rotate(0deg)", "height": "35px", "width": "14px", "top": "1px", "border-radius": "0 50% 50% 50%" })
+
+//###################################################
+//TEETH FUNCTION
+//###################################################
+function teethVariation(num) {
+    
+    $('#dnateeth').html(num)
+    switch (num) {
+        case 1:
+            normalTeeth()
+            $('#TeethName').html('Normal') //set the badge to "n0rmal"
+            break
+        case 2:
+            normalTeeth() //reset 
+            $('#TeethName').html('Crooked')
+            TeethType2() //set border type
+            break
+        case 3:
+            normalTeeth() //reset 
+            $('#TeethName').html('Large')
+            TeethType3() //set border type
+            break
+        case 4:
+            normalTeeth() //reset 
+            $('#TeethName').html('Laughing')
+            TeethType4() //set border type
+            break
+        case 5:
+            normalTeeth() //reset 
+            $('#TeethName').html('Tiny')
+            TeethType5() //set border type
+            break
+    }
 }
+
+function normalTeeth() {
+    $('.rightTooth, .leftTooth').css('transform', 'none')
+    $('.rightTooth, .leftTooth').css('top', '0px')
+    $('.rightTooth').css('left', '50px')
+    $('.leftTooth').css('left', '25px')
+    $('.rightTooth, .leftTooth').css('height', '40px')
+    $('.rightTooth, .leftTooth').css('width', '25px')
+}
+function TeethType2() {
+    $('.rightTooth, .leftTooth').css('transform', 'rotate(-7deg)')
+    $('.rightTooth, .leftTooth').css('top', '5px')
+    $('.rightTooth').css('left', '60px')
+    $('.leftTooth').css('left', '20px')
+}
+function TeethType3() {
+    $('.rightTooth, .leftTooth').css('height', '30px')
+    $('.rightTooth, .leftTooth').css('width', '35px')
+    $('.rightTooth').css('left', '50px')
+    $('.leftTooth').css('left', '15px')
+}
+function TeethType4() {
+    $('.rightTooth, .leftTooth').css('transform', 'skew(30deg) rotate(0deg) scaleX(.9) scaleY(.9)')
+    $('.rightTooth').css('left', '55px')
+    $('.leftTooth').css('left', '30px')
+    $('.rightTooth, .leftTooth').css('height', '25px')
+}
+function TeethType5() {
+    $('.rightTooth, .leftTooth').css('transform', 'skew(0deg) rotate(0deg) scaleX(.2) scaleY(.2)')
+    $('.rightTooth').css('left', '45px')
+    $('.leftTooth').css('left', '20px')
+    $('.rightTooth, .leftTooth').css('top', '-13px')
+    $('.rightTooth, .leftTooth').css('width', '35px')
+
+}
+
+
+
--- assets/js/catSettings.js
+++ assets/js/catSettings.js
@@ -2,46 +2,44 @@
 var colors = Object.values(allColors()) 
 
 var defaultDNA = {
-    "headBellyColor" : 10,
     "bodyLimbsColor" : 13,
-    "tailEarsColor" : 96,
+    "tailBellyColor" : 96,
+    "surfBoardColor" : 14,
     "bgColor" : 90,
     //Cattributes
     "eyesShape" : 1,
-    "decorationPattern" : 1,
-    "decorationMidcolor" : 13,
-    "decorationSidescolor" : 13,
-    "animation" :  1,
+    "tailspinShape" : 1,
+    "teethShape" : 1,
+
+    "animation" :  8,
     "lastNum" :  1
     }
 
 // when page load
 $( document ).ready(function() {
-  $('#dnahead').html(defaultDNA.headBellyColor);
   $('#dnabody').html(defaultDNA.bodyLimbsColor);
-  $('#dnatail').html(defaultDNA.tailEarsColor);
+  $('#dnatail').html(defaultDNA.tailBellyColor);
+  $('#dnasurf').html(defaultDNA.surfBoardColor);
   $('#dnabg').html(defaultDNA.bgColor);
-    
-//   $('#dnashape').html(defaultDNA.eyesShape)
-//   $('#dnadecoration').html(defaultDNA.decorationPattern)
-//   $('#dnadecorationMid').html(defaultDNA.decorationMidcolor)
-//   $('#dnadecorationSides').html(defaultDNA.decorationSidescolor)
-//   $('#dnaanimation').html(defaultDNA.animation)
-//   $('#dnaspecial').html(defaultDNA.lastNum)
+  $('#dnaeyes').html(defaultDNA.eyesShape);
+  $('#dnatailspin').html(defaultDNA.tailspinShape);
+  $('#dnateeth').html(defaultDNA.teethShape);
+
+   $('#dnaanimation').html(defaultDNA.animation);
+   $('#dnaspecial').html(defaultDNA.lastNum);
 
   renderCat(defaultDNA)
 });
 
 function getDna(){
     var dna = ''
-    dna += $('#dnahead').html()
     dna += $('#dnabody').html()
     dna += $('#dnatail').html()
+    dna += $('#dnasurf').html()
     dna += $('#dnabg').html()
-    dna += $('#dnashape').html()
-    dna += $('#dnadecoration').html()
-    dna += $('#dnadecorationMid').html()
-    dna += $('#dnadecorationSides').html()
+    dna += $('#dnaeyes').html()
+    dna += $('#dnatailspin').html()
+    dna += $('#dnateeth').html()
     dna += $('#dnaanimation').html()
     dna += $('#dnaspecial').html()
 
@@ -49,25 +47,39 @@ function getDna(){
 }
 
 function renderCat(dna){
-    headColor(colors[dna.headBellyColor],dna.headBellyColor)
-    $('#headBellyColor').val(dna.headBellyColor)
-
     bodyLimbsColor(colors[dna.bodyLimbsColor],dna.bodyLimbsColor)
     $('#bodyLimbsColor').val(dna.bodyLimbsColor)
 
-    tailEarsColor(colors[dna.tailEarsColor],dna.tailEarsColor)
-    $('#tailEarsColor').val(dna.tailEarsColor)
+    tailBellyColor(colors[dna.tailBellyColor],dna.tailBellyColor)
+    $('#tailBellyColor').val(dna.tailBellyColor)
+
+    surfBoardColor(colors[dna.surfBoardColor],dna.surfBoardColor)
+    $('#surfBoardColor').val(dna.surfBoardColor)
 
     bgColor(colors[dna.bgColor],dna.bgColor)
     $('#bgColor').val(dna.bgColor)
+
+    eyeVariation(dna.eyesShape)
+    $('#eyesShape').val(dna.eyesShape)
+    var eyeShapeText = ($('#eyesShape').val())
+    $('#eyeShapeText').html(eyeShapeText)
+    console.log(eyeShapeText)
+
+    tailspinVariation(dna.tailspinShape)
+    $('#tailspinShape').val(dna.tailspinShape)
+    var tailspinShapeText = ($('#tailspinShape').val())
+    $('#tailspinShapeText').html(tailspinShapeText)
+    console.log(tailspinShapeText)
+
+    teethVariation(dna.teethShape)
+    $('#teethShape').val(dna.teethShape)
+    var teethShapeText = ($('#teethShape').val())
+    $('#teethShapeText').html(teethShapeText)
+    console.log(teethShapeText)
+
 }
 
-// Changing cat colors
-$('#headBellyColor').change(()=>{
-    console.log( $('#headBellyColor').val() )
-    var colorVal = $('#headBellyColor').val()
-    headColor(colors[colorVal],colorVal)
-})
+// Changing squirrel colors
 
 $('#bodyLimbsColor').change(()=>{
   console.log( $('#bodyLimbsColor').val() )
@@ -75,14 +87,47 @@ $('#bodyLimbsColor').change(()=>{
   bodyLimbsColor(colors[colorVal],colorVal)
 })
 
-$('#tailEarsColor').change(()=>{
-  console.log( $('#tailEarsColor').val() )
-  var colorVal = $('#tailEarsColor').val()
-  tailEarsColor(colors[colorVal],colorVal)
+$('#tailBellyColor').change(()=>{
+  console.log( $('#tailBellyColor').val() )
+  var colorVal = $('#tailBellyColor').val()
+  tailBellyColor(colors[colorVal],colorVal)
+})
+
+$('#surfBoardColor').change(()=>{
+  console.log( $('#surfBoardColor').val() )
+  var colorVal = $('#surfBoardColor').val()
+  surfBoardColor(colors[colorVal],colorVal)
 })
 
 $('#bgColor').change(()=>{
   console.log( $('#bgColor').val() )
   var colorVal = $('#bgColor').val()
   bgColor(colors[colorVal],colorVal)
+})
+
+
+// Changing squirrel shape
+
+$('#eyesShape').change(()=>{
+  console.log( $('#eyesShape').val() )
+  var shape = parseInt($('#eyesShape').val()) //between 1 and 9
+  eyeVariation(shape)
+  var eyeShapeText = ($('#eyesShape').val())
+  $('#eyeShapeText').html(eyeShapeText)
+})
+
+$('#tailspinShape').change(()=>{
+  console.log( $('#tailspinShape').val() )
+  var shape = parseInt($('#tailspinShape').val()) //between 1 and 9
+  tailspinVariation(shape)
+  var tailspinShapeText = ($('#tailspinShape').val())
+  $('#tailspinShapeText').html(tailspinShapeText)
+})
+
+$('#teethShape').change(()=>{
+  console.log( $('#teethShape').val() )
+  var shape = parseInt($('#teethShape').val()) //between 1 and 9
+  teethVariation(shape)
+  var teethShapeText = ($('#teethShape').val())
+  $('#teethShapeText').html(teethShapeText)
 })
