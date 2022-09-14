542911f43af570bb7f2d52a3f048e08aac7fdca0
CipherGrind <101846763+CipherGrind@users.noreply.github.com>
2022-09-14 13:57:53 -0400

    update3c

--- assets/js/catFactory.js
+++ assets/js/catFactory.js
@@ -381,31 +381,31 @@ function coinVariation(num) {
 }
 
 function CoinType1() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/BTC-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/BTC-100px.png)')
 }
 function CoinType2() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/ETH1-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/ETH-100px.png)')
 }
 function CoinType3() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/POLYGON-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/POLYGON-100px.png)')
 }
 function CoinType4() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/SOL-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/SOL-100px.png)')
 }
 function CoinType5() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/AVAX-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/AVAX-100px.png)')
 }
 function CoinType6() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/ALGO-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/ALGO-100px.png)')
 }
 function CoinType7() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/BNB-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/BNB-100px.png)')
 }
 function CoinType8() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/HBAR-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/HBAR-100px.png)')
 }
 function CoinType9() {
-    $('.coin1').css('background-image', 'url(https://github.com/CipherGrind/TrippySquirrelsNFTMaker/blob/gh-pages/assets/images/DOT-100px.png)')
+    $('.coin1').css('background-image', 'url(./assets/images/DOT-100px.png)')
 }
 
 
